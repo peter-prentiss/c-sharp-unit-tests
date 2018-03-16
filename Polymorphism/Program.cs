99c88a699d55b7cf48ae152370ffbb106c589e9d
Peter <peter.prentiss@gmail.com>
2018-03-15 23:42:14 -0500

    Updates

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+
+namespace Polymorphism
+{
+    public class Employee
+    {
+        public virtual string CalculateWeeklySalary(int weeklyHours, int wage)
+        {
+            var salary = 40 * wage;
+            string result = String.Format("This ANGRY EMPLOYEE worked {0} hrs. " +
+                            "Paid for 40 hrs at $ {1}" +
+                            "/hr = ${2}", weeklyHours, wage, salary);
+            Console.WriteLine("\n" + result + " \n");
+            Console.WriteLine("---------------------------------------------\n");
+            return result;
+        }
+    }
+
+    public class Contractor : Employee
+    {
+        public override string CalculateWeeklySalary(int weeklyHours, int wage)
+        {
+            var salary = weeklyHours * wage;
+            string result = String.Format("This HAPPY CONTRACTOR worked {0} hrs. " +
+                            "Paid for {0} hrs at $ {1}" +
+                            "/hr = ${2} ", weeklyHours, wage, salary);
+            Console.WriteLine("\n" + result + " \n");
+            return result;
+        }
+    }
+
+
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int hours = 55, wage = 70;
+            //List<Employee> employees = Utils.GetEmployees();
+
+            var mock = new Mock<Utils>();
+            mock.Setup(m => m.GetMockEmployees()).Returns(() => new List<Employee> { new Contractor(), new Employee() });
+
+            List<Employee> employees = mock.Object.GetMockEmployees();
+
+            foreach (var e in employees)
+            {
+                e.CalculateWeeklySalary(hours, wage);
+            }
+        }
+
+
+    }
+
+    public static class Utils {
+        //public static List<Employee> GetEmployees()
+        //{
+        //    var someEmployee = new Employee();
+        //    var someContractor = new Contractor();
+        //    var everyone = new List<Employee> { someEmployee, someContractor };
+        //    return everyone;
+        //}
+
+        public virtual List<Employee> GetMockEmployees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PolymorphismTestProject/UnitTest1.cs
@@ -0,0 +1,75 @@
+using System;
+using Polymorphism;
+using Xunit;
+
+namespace PolymorphismTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CalculateWeeklySalaryForEmployeeTest_70wage55hoursReturns2800dollars()
+        {
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = 40 * wage;
+            Employee e = new Employee();
+
+            string expectedResponse = String.Format($"This ANGRY EMPLOYEE worked {weeklyHours} hrs. " +
+                                                    $"Paid for 40 hrs at $ {wage}" +
+                                                    $"/hr = ${salary}");
+
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+            Assert.Equal(response, expectedResponse);
+        }
+
+        [Fact]
+        public void CalculateWeeklySalaryForContractorTest_70wage55hoursReturns3850dollars()
+        {
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = weeklyHours * wage;
+            Employee e = new Contractor();
+
+            string expectedResponse = String.Format($"This HAPPY CONTRACTOR worked {weeklyHours} hrs. " +
+                                                    $"Paid for {weeklyHours} hrs at $ {wage}" +
+                                                    $"/hr = ${salary} ");
+
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+            Assert.Equal(response, expectedResponse);
+        }
+
+        public void CalculateWeeklySalaryForEmployeeTest_70wage55hoursDoesNotReturnCorrectString()
+        {
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = 40 * wage;
+            Employee e = new Employee();
+
+            string expectedResponse = String.Format($"Problem 1-This ANGRY EMPLOYEE worked {weeklyHours} hrs. " +
+                                                    $"Paid for 40 hrs at $ {wage}" +
+                                                    $"/hr = ${salary}");
+
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+            Assert.NotEqual(response, expectedResponse);
+        }
+
+        public void CalculateWeeklySalaryForContractorTest_70wage55hoursDoesNotReturnCorrectString()
+        {
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = weeklyHours * wage;
+            Employee e = new Contractor();
+
+            string expectedResponse = String.Format($"Problem 2-This HAPPY CONTRACTOR worked {weeklyHours} hrs. " +
+                                                    $"Paid for {weeklyHours} hrs at $ {wage}" +
+                                                    $"/hr = ${salary} ");
+
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+            Assert.NotEqual(response, expectedResponse);
+        }
+    }
+}
